7c65040d612f2699799c606fe06cb70f60d661d7
Sandeep Parmar <sandy02180@gmail.com>
2021-06-24 13:21:11 +0530

    ch6_cmpt

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,6 +27,15 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 @login.user_loader
 def load_user(id):
